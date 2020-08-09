921c37e16336ab5e6e76015e7dba0906b6185ca6
Zeljko <zeljki.markovic15@gmail.com>
2020-08-09 03:31:50 +0200

    password strength bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './auth/login/login.component';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,11 +17,12 @@ import { ToastrModule } from 'ngx-toastr';
     AppComponent,
     HeaderComponent,
     SignupComponent,
-    LoginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgxWebstorageModule.forRoot(),
@@ -31,4 +32,4 @@ import { ToastrModule } from 'ngx-toastr';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SignupRequest } from './signupRequest';
 import { AuthService } from '../shared/auth.service';
@@ -13,6 +13,10 @@ import { Router } from '@angular/router';
 export class SignupComponent implements OnInit {
   signupRequestPayload: SignupRequest;
   signupForm: FormGroup;
+  public account = {
+    password: null
+  };
+  public barLabel: string = 'Password strength:';
 
   constructor(
     private authService: AuthService,
@@ -39,9 +43,88 @@ export class SignupComponent implements OnInit {
     this.signupRequestPayload.username = this.signupForm.get('username').value;
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
-    this.authService.signup(this.signupRequestPayload).subscribe((error) => {
-      console.log(error);
-      this.toastr.error('Registration Failed! Please try again');
-    });
+    this.authService.signup(this.signupRequestPayload).subscribe(
+      (data) => {
+        this.router.navigate(['/login'], {
+          queryParams: { registered: 'true' },
+        });
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error('Registration Failed! Please try again');
+      }
+    );
+  }
+
+  // password strength bar
+  @Input() passwordToCheck: string;
+
+  bar0: string;
+  bar1: string;
+  bar2: string;
+  bar3: string;
+  bar4: string;
+
+  private colors = ['#F00', '#F90', '#FF0', '#9F0', '#0F0'];
+  private static measureStrength(pass: string) {
+    let score = 0;
+    // award every unique letter until 5 repetitions
+    let letters = {};
+    for (let i = 0; i < pass.length; i++) {
+      letters[pass[i]] = (letters[pass[i]] || 0) + 1;
+      score += 5.0 / letters[pass[i]];
+    }
+
+    // bonus points for mixing it up
+
+    let variations = {
+      digits: /\d/.test(pass),
+      lower: /[a-z]/.test(pass),
+      upper: /[A-Z]/.test(pass),
+      nonWords: /\W/.test(pass),
+    };
+
+    let variationCount = 0;
+    for (let check in variations) {
+      variationCount += variations[check] ? 1 : 0;
+    }
+    score += (variationCount - 1) * 10;
+    return Math.trunc(score);
+  }
+
+  private getColor(score: number) {
+    let idx = 0;
+    if (score > 90) {
+      idx = 4;
+    } else if (score > 70) {
+      idx = 3;
+    } else if (score >= 40) {
+      idx = 2;
+    } else if (score >= 20) {
+      idx = 1;
+    }
+
+    return {
+      idx: idx + 1,
+      col: this.colors[idx],
+    };
+  }
+
+  onChangeEvent(event: any) {
+
+    var password = event;
+    this.setBarColors(5, '#DDD');
+    if (password) {
+      let c = this.getColor(
+        SignupComponent.measureStrength(password)
+      );
+      this.setBarColors(c.idx, c.col);
+    }
+  }
+
+  private setBarColors(count, col) {
+    for (let _n = 0; _n < count; _n++) {
+      this['bar' + _n] = col;
+    }
   }
 }
