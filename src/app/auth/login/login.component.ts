61c7aac441ac9b622a87a884f6975f952c0e6137
Zeljko <zeljki.markovic15@gmail.com>
2020-08-25 23:38:35 +0200

    phase 1 fix

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -49,9 +49,6 @@ export class LoginComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe((params) => {
       if (params.registered !== undefined && params.registered === 'true') {
         this.toastr.success('Signup Successful');
-        this.registerSuccessMessage =
-          'Please Check your inbox for activation email ' +
-          'activate your account before you Login!';
       }
     });
   }
